493b3380016a2062335899746be1149a538c8649
Calvin Loncaric <marvinx03@gmail.com>
2022-11-15 10:52:03 -0800

    Add tlapm

---
+++ Formula/tlapm@202210041448.rb
@@ -0,0 +1,46 @@
+class TlapmAT202210041448 < Formula
+  desc "The TLA+ Proof Manager"
+  homepage "https://tla.msr-inria.inria.fr/tlaps/"
+  url "https://github.com/tlaplus/tlapm/archive/202210041448.tar.gz"
+  version "202210041448"
+  sha256 "c6c046f8cfc211bfee912bd6d6d736d9375411c7dad109bd3651c748e0d5550c"
+  license "BSD2"
+
+  depends_on "ocaml" => :build
+
+  keg_only "the `tlaps` package installs a `tlapm` wrapper script with better defaults"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install", "-j1"
+  end
+
+  def caveats
+    <<~EOS
+      You probably want to install the `tlaps` package, not this one!
+
+      This formula only includes the `tlapm` executable.  `tlapm` discovers
+      external solvers using your $PATH environment variable, so its behavior
+      is quite system-dependent.  The `tlaps` package includes the default set
+      of external solvers and installs a wrapper script so that `tlapm` does
+      not depend so much on your environment.
+
+      This formula exists in case you want to use the raw `tlapm` program,
+      unadulterated.
+    EOS
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test ezpsl`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
