00e489867940d50be161d06c4dabffa79a23032c
Calvin Loncaric <marvinx03@gmail.com>
2023-02-02 14:30:39 -0800

    Update CoqHammer for Coq 8.16
    This commit also splits CoqHammer into two formulae; the "plugin" part
    won't build until the "tactics" part is fully installed.
    
    Along the way I had to manually specify many paths that "just worked"
    before.  I do not know if this was due to changes in Coq 8.16 or due to
    changes in Homebrew's formula definitions, but someone somewhere is
    resolving symlinks when they probably shouldn't...

---
+++ Formula/coqhammer-tactics@1.3.2.rb
@@ -0,0 +1,56 @@
+class CoqhammerTacticsAT132 < Formula
+  desc "An Automated Reasoning Hammer Tool for Coq"
+  homepage "https://coqhammer.github.io/"
+  url "https://github.com/lukaszcz/coqhammer/archive/refs/tags/v1.3.2+8.16.tar.gz"
+  version "1.3.2"
+  sha256 "5844ff4cd093c22f103f39407c97ef6d70cbbff6e19a19302bb908d3177e660c"
+  license "LGPL2"
+  revision 1
+
+  depends_on "ocaml" => :build
+  depends_on "ocaml-findlib" => :build
+  depends_on "coq"
+
+  def install
+    # NOTE 2023/2/2: Coq 8.16 (or maybe Homebrew's Coq?) installs lib files
+    # directly to lib/ instead of lib/ocaml/ like it used to.  Ocamlfind can't
+    # find things there without help.
+    ENV.prepend_path "OCAMLPATH", Formula["coq"].lib
+
+    # NOTE 2023/2/2 regarding COQPLUGININSTALL: Coq 8.16 searches for native
+    # ("plugin") libraries in
+    #
+    #   HOMEBREW_PREFIX/lib/coq/../coq-core/..
+    #
+    # Unfortunately, this interacts poorly with Homebrew's symlink mechanism;
+    # that path resolves to
+    #
+    #   HOMEBREW_PREFIX/Cellar/coq/8.16.1/lib
+    #
+    # In other words, we would have to install the native libraries in Coq's
+    # private install folder for this to work.  Fortunately there is a
+    # workaround: Coq also appears to search
+    #
+    #   HOMEBREW_PREFIX/lib/ocaml
+    #
+    # so we'll use that path.
+    system "make", "tactics", "install-tactics",
+      "COQLIBINSTALL=#{lib}/coq/user-contrib/",
+      "COQPLUGININSTALL=#{lib}/ocaml/",
+      "COQDOCINSTALL=#{share}/share/coq/user-contrib/",
+      "BINDIR=#{bin}/"
+  end
+
+  def caveats
+    <<~EOS
+      This formula installs the CoqHammer tactics only! For functionality
+      provided by the plugin, install `coqhammer` (which depends on this
+      package).
+    EOS
+  end
+
+  test do
+    (testpath/"Test.v").write "From Hammer Require Import Tactics."
+    system "coqc", "Test.v"
+  end
+end
--- Formula/coqhammer@1.3.2.rb
+++ Formula/coqhammer@1.3.2.rb
@@ -1,26 +1,61 @@
 class CoqhammerAT132 < Formula
   desc "An Automated Reasoning Hammer Tool for Coq"
   homepage "https://coqhammer.github.io/"
-  url "https://github.com/lukaszcz/coqhammer/archive/refs/tags/v1.3.2-coq8.15.tar.gz"
+  url "https://github.com/lukaszcz/coqhammer/archive/refs/tags/v1.3.2+8.16.tar.gz"
   version "1.3.2"
-  sha256 "d334417f5934fc222578382977e66f7eccb7f2e6b8d9f6570d5afe1a878ca70f"
+  sha256 "5844ff4cd093c22f103f39407c97ef6d70cbbff6e19a19302bb908d3177e660c"
   license "LGPL2"
-  revision 2
+  revision 3
 
   depends_on "ocaml" => :build
   depends_on "ocaml-findlib" => :build
   depends_on "coq"
+  depends_on "coqhammer-tactics@1.3.2"
 
   def install
-    system "make"
-    system "make", "install",
-      "COQLIBINSTALL=#{prefix}/lib/coq/user-contrib",
-      "COQDOCINSTALL=#{prefix}/share/coq/user-contrib",
-      "BINDIR=#{prefix}/bin/"
+    # NOTE 2023/2/2: Coq 8.16 (or maybe Homebrew's Coq?) installs lib files
+    # directly to lib/ instead of lib/ocaml/ like it used to.  Ocamlfind can't
+    # find things there without help.
+    ENV.prepend_path "OCAMLPATH", Formula["coq"].lib
+
+    # NOTE 2023/2/2: See note in coqhammer-tactics about COQPLUGININSTALL.
+
+    # NOTE 2023/2/2: Makefile can't find coqhammer-tactics because $PATH
+    # includes
+    #
+    #   /opt/homebrew/opt/coq/bin
+    #
+    # ahead of other entries.  I guess Coq searches relative to itself to find
+    # its library, and everything falls apart!  So for this build, where
+    # coqc is invoked using this special path instead of
+    # HOMEBREW_PREFIX/bin, we need to point Coq at the right things.
+    # This path has to match COQLIBINSTALL from the coqhammer-tactics formula.
+    ENV.prepend_path "COQPATH", Formula["coqhammer-tactics@1.3.2"].lib/"coq/user-contrib"
+
+    # NOTE 2023/2/2: It seems that Coq 8.16 fully resolves its path when
+    # generating makefiles, so it writes something like
+    #
+    #   COQMF_COQLIB=/opt/homebrew/Cellar/coq/8.16.1/lib/coq/
+    #   COQMF_COQCORELIB=/opt/homebrew/Cellar/coq/8.16.1/lib/coq/../coq-core/
+    #   COQMF_DOCDIR=/opt/homebrew/Cellar/coq/8.16.1/share/coq/latex/
+    #
+    # This is wrong---3rd party libraries are not installed to that directory,
+    # only the core Coq libraries.  Homebrew symlinks everything to its own
+    # prefix, e.g. /opt/homebrew/lib/coq.
+    #
+    # Fortunately we can override those variables during the build.
+    system "make", "plugin", "install-plugin",
+      "COQMF_COQLIB=#{HOMEBREW_PREFIX}/lib/coq/",
+      "COQMF_COQCORELIB=#{HOMEBREW_PREFIX}/lib/coq-core/",
+      "COQMF_DOCDIR=#{HOMEBREW_PREFIX}/share/coq/latex/",
+      "COQLIBINSTALL=#{lib}/coq/user-contrib/",
+      "COQPLUGININSTALL=#{lib}/ocaml/",
+      "COQDOCINSTALL=#{share}/coq/user-contrib/",
+      "BINDIR=#{bin}/"
   end
 
   test do
-    (testpath/"Test.v").write "From Hammer Require Import Tactics."
+    (testpath/"Test.v").write "From Hammer Require Import Hammer."
     system "coqc", "Test.v"
   end
 end
