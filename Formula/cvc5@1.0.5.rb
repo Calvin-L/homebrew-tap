f4410bf60dde638bfd47468d8d7f7164f7b099ca
Calvin Loncaric <marvinx03@gmail.com>
2023-04-02 14:50:12 -0700

    Add cvc5 1.0.5

---
+++ Formula/cvc5@1.0.5.rb
@@ -0,0 +1,41 @@
+class Cvc5AT105 < Formula
+  desc "cvc5 is an open-source automatic theorem prover for Satisfiability Modulo Theories (SMT) problems."
+  homepage "https://cvc5.github.io/"
+  version "1.0.5"
+  url "https://github.com/cvc5/cvc5/archive/cvc5-#{version}.tar.gz"
+  sha256 "a9705569fe36c70291dd1eb6dc5f542d33da51f82da46558e3455ed6995b1b7a"
+  license "BSD3"
+
+  depends_on "cmake" => :build
+  depends_on "cadical" => :build
+  depends_on "python@3.10" => :build
+  depends_on "python3-toml@0.10.2" => :build
+  depends_on "python3-pyparsing@3.0.9" => :build
+  depends_on "java" => :build
+  depends_on "symfpu@2019.5.17" => :build # header-only library
+  depends_on "antlr@3.4" => :build
+  depends_on "libantlr3c"
+  depends_on "gmp"
+  depends_on "libpoly"
+
+  def install
+    system "./configure.sh", "production", "--poly", "--prefix=#{prefix}"
+    cd "build" do
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test ezpsl`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
--- Formula/python3-pyparsing@3.0.9.rb
+++ Formula/python3-pyparsing@3.0.9.rb
@@ -1,8 +1,8 @@
-class Python3TomlAT0102 < Formula
-  desc "Python lib for TOML"
-  homepage "https://github.com/uiri/toml"
-  url "https://files.pythonhosted.org/packages/be/ba/1f744cdc819428fc6b5084ec34d9b30660f6f9daaf70eead706e3203ec3c/toml-0.10.2.tar.gz"
-  sha256 "b3bda1d108d5dd99f4a20d24d9c348e91c4db7ab1b749200bded2f839ccbe68f"
+class Python3PyparsingAT309 < Formula
+  desc "Classes and methods to define and execute parsing grammars"
+  homepage "https://github.com/pyparsing/pyparsing/"
+  url "https://files.pythonhosted.org/packages/71/22/207523d16464c40a0310d2d4d8926daffa00ac1f5b1576170a32db749636/pyparsing-3.0.9.tar.gz"
+  sha256 "2b020ecf7d21b687f219b71ecad3631f644a47f01403fa1d1036b0c6416d70fb"
   license "MIT"
   revision 1
 
@@ -16,6 +16,14 @@ class Python3TomlAT0102 < Formula
   end
 
   def install
+    (Pathname.pwd/"setup.py").write <<~EOS
+      from setuptools import setup
+
+      setup(name="pyparsing",
+            description="#{desc}",
+            version="#{version}",
+      )
+    EOS
     pythons.each do |python|
       system python.opt_libexec/"bin"/"python", *Language::Python.setup_install_args(prefix, python)
     end
@@ -27,15 +35,15 @@ class Python3TomlAT0102 < Formula
                              .join(", ")
 
     <<~EOS
-      This formula provides the `toml` module for Python #{python_versions}.
-      If you need `toml` for a different version of Python, use pip.
+      This formula provides the `pyparsing` module for Python #{python_versions}.
+      If you need `pyparsing` for a different version of Python, use pip.
     EOS
   end
 
   test do
     pythons.each do |python|
       system python.opt_libexec/"bin"/"python", "-c", <<~EOS
-        import toml
+        import pyparsing
       EOS
     end
   end
