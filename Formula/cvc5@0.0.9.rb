954521b322b16c6a36c9bfabbc03a6de260bb839
Calvin Loncaric <marvinx03@gmail.com>
2023-04-02 15:49:22 -0700

    Remove libpoly from cvc5 builds
    The package of libpoly that I have been using is apparently from a 3rd
    party tap (sri-csl/sri-csl/libpoly).  It is too old, as described here:
    https://github.com/cvc5/cvc5/issues/7993

--- Formula/cvc5@0.0.9.rb
+++ Formula/cvc5@0.0.9.rb
@@ -5,6 +5,7 @@ class Cvc5AT009 < Formula
   version "0.0.9"
   sha256 "95bcc332d8855bba2669bf73d0cbf54c5b716fa9f1867997c6589bc4acd4e65e"
   license "BSD3"
+  revision 1
 
   depends_on "cmake" => :build
   depends_on "cadical" => :build
@@ -15,14 +16,22 @@ class Cvc5AT009 < Formula
   depends_on "antlr@3.4" => :build
   depends_on "libantlr3c"
   depends_on "gmp"
-  depends_on "libpoly"
 
   def install
-    system "./configure.sh", "production", "--poly", "--prefix=#{prefix}"
+    system "./configure.sh", "production", "--no-poly", "--prefix=#{prefix}"
     cd "build" do
       system "make"
       system "make", "install"
     end
+  end
+
+  def caveats
+    <<~EOS
+      This cvc5 package is compiled WITHOUT libpoly support, so certain
+      nonlinear reasoning features might not be available.  Today (2023/4/2)
+      there is no sufficiently-up-to-date package of libpoly available.  See
+      https://github.com/cvc5/cvc5/issues/7993
+    EOS
   end
 
   test do
