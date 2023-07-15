52a47d38cccee7ebacda728c7de1c753193590ba
Calvin Loncaric <marvinx03@gmail.com>
2023-07-15 10:49:56 -0700

    Add cvc4 to tlaps

--- Formula/tlaps@202210041448.rb
+++ Formula/tlaps@202210041448.rb
@@ -5,23 +5,28 @@ class TlapsAT202210041448 < Formula
   version "202210041448"
   sha256 "c6c046f8cfc211bfee912bd6d6d736d9375411c7dad109bd3651c748e0d5550c"
   license "BSD2"
-  revision 2
+  revision 3
 
   depends_on "tlapm@#{version}"
   depends_on "ls4"
   depends_on "z3"
-  # depends_on "cvc4"
+  depends_on "cvc5@1.0.5"
+  # depends_on "sri-csl/sri-csl/yices2" # NOTE 2023/7/15: currently broken
   depends_on "zenon"
   depends_on "ptl-to-trp-translator"
   depends_on "isabelle@2011-1"
   depends_on "tlaps-isabelle-heap@#{version}"
 
   def install
+    # install fake cvc4 (just a symlink to cvc5)
+    mkdir_p libexec
+    ln_s "#{Formula["cvc5@1.0.5"].bin}/cvc5", "#{libexec}/cvc4"
+
     (bin/"tlapm").write_env_script "#{Formula["tlapm"].bin}/tlapm",
         PATH: [
           "#{Formula["ls4"].bin}",
           "#{Formula["z3"].bin}",
-          # "#{Formula["cvc4"].bin}",
+          "#{libexec}",
           "#{Formula["zenon"].bin}",
           "#{Formula["ptl-to-trp-translator"].bin}",
           "#{Formula["isabelle@2011-1"].bin}",
