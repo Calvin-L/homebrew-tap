d813dc44b15d9e81b93c963e7ec49c26f7d13b5e
Calvin Loncaric <marvinx03@gmail.com>
2023-07-25 23:26:38 -0700

    Use boolector from homebrew-core

--- Formula/many-smt.rb
+++ Formula/many-smt.rb
@@ -4,11 +4,12 @@ class ManySmt < Formula
   url "https://github.com/Calvin-L/many-smt/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "b33902cb95edb437e453c81501cd8251290a3d9b748d9d4e83a40736fde2705f"
   license "MIT"
+  revision 1
 
   depends_on "python@3"
   depends_on "calvin-l/tap/cvc5" => :recommended
   depends_on "z3" => :recommended
-  depends_on "mht208/formal/boolector" => :optional
+  depends_on "boolector" => :optional
   depends_on "sri-csl/sri-csl/yices2" => :optional
 
   def install
