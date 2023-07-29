c3916fa34b1437cac01f210998bea47b103f6ffe
Calvin Loncaric <marvinx03@gmail.com>
2023-07-29 15:18:27 -0700

    Improve lowdown formula and add a test

--- Formula/lowdown@0.11.2.rb
+++ Formula/lowdown@0.11.2.rb
@@ -4,9 +4,15 @@ class LowdownAT0112 < Formula
   url "https://github.com/kristapsdz/lowdown/archive/VERSION_0_11_2.tar.gz"
   sha256 "96773100f5d03402c8df210a10725172515cd1c9334c7c5b6e7603e0c8810124"
   license "ISC"
+  revision 1
 
   def install
-    system "./configure", "PREFIX=#{prefix}"
+    inreplace "configure",
+      "HAVE_SANDBOX_INIT=",
+      "HAVE_SANDBOX_INIT=0" # sandbox causes runtime errors (it's deprecated anyway)
+    system "./configure",
+      "PREFIX=#{prefix}",
+      "MANDIR=#{man}" # by default, lowdown incorrectly installs to prefix/man instead of prefix/share/man
     inreplace "Makefile",
       "-Wl,-soname,$@.$(LIBVER)",
       "-Wl,-install_name,liblowdown.1.dylib" # otherwise fails with "ld: unknown option: -soname"
@@ -17,15 +23,20 @@ class LowdownAT0112 < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test ezpsl`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    (testpath/"test.md").write <<~EOS
+      # Readme
+      _text_
+    EOS
+    system bin/"lowdown", testpath/"test.md", "-o", testpath/"test.html"
+
+    (testpath/"test.c").write <<~EOS
+      #include <sys/queue.h>
+      #include <stdio.h>
+      #include <lowdown.h>
+
+      int main(int argc, char** argv) {
+      }
+    EOS
+    system ENV.cc, "-I#{include}", "-L#{lib}", "-llowdown", testpath/"test.c", "-o", testpath/"a.out"
   end
 end
