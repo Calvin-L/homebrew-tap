45ea72f05ea746e8c8416f92f2d425f2e5930ed8
Calvin Loncaric <marvinx03@gmail.com>
2023-07-29 15:12:09 -0700

    Add a test and improved configuration for libeditline

--- Formula/libeditline@1.17.1.rb
+++ Formula/libeditline@1.17.1.rb
@@ -4,23 +4,32 @@ class LibeditlineAT1171 < Formula
   url "https://github.com/troglobit/editline/releases/download/1.17.1/editline-1.17.1.tar.gz"
   sha256 "781e03b6a935df75d99fb963551e2e9f09a714a8c49fc53280c716c90bf44d26"
   license "Spencer-94"
+  revision 1
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", *std_configure_args
     system "make"
     system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test ezpsl`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <stdlib.h>
+      #include <editline.h>
+
+      int main(int argc, char** argv) {
+          char *p;
+
+          while ((p = readline("CLI> ")) != NULL) {
+              puts(p);
+              free(p);
+          }
+
+          return 0;
+      }
+    EOS
+
+    system ENV.cc, "-I#{include}", "-L#{lib}", "-leditline", testpath/"test.c", "-o", testpath/"a.out"
   end
 end
