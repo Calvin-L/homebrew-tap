c6e0e8a460bd87a2d8fb1ca0d41af277c43bdc1c
Calvin Loncaric <marvinx03@gmail.com>
2023-07-29 16:04:13 -0700

    Add a test for symfpu

--- Formula/symfpu@2019.5.17.rb
+++ Formula/symfpu@2019.5.17.rb
@@ -13,15 +13,14 @@ class SymfpuAT2019517 < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test ezpsl`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    (testpath/"test.cpp").write <<~EOS
+      #include <symfpu/core/unpackedFloat.h>
+      #include <symfpu/core/packing.h>
+      #include <symfpu/core/add.h>
+
+      int main(int argc, char** argv) {
+      }
+    EOS
+    system ENV.cxx, "-I#{include}", testpath/"test.cpp", "-o", testpath/"a.out"
   end
 end
