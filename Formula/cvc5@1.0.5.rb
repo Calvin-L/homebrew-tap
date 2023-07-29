a6b71d34337f99557a525933ab4e30d8b8491442
Calvin Loncaric <marvinx03@gmail.com>
2023-07-29 14:58:12 -0700

    Add trivial tests for cvc5

--- Formula/cvc5@1.0.5.rb
+++ Formula/cvc5@1.0.5.rb
@@ -27,15 +27,6 @@ class Cvc5AT105 < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test ezpsl`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system bin/"cvc5", "--version"
   end
 end
