22f0ce2d7b2e6acdda48fc7d5f72dcd402aedd66
Calvin Loncaric <marvinx03@gmail.com>
2023-09-23 16:17:30 -0700

    Upgrade CalTac

--- Formula/caltac.rb
+++ Formula/caltac.rb
@@ -1,10 +1,11 @@
 class Caltac < Formula
   desc "Calvin's basic Coq tactics"
   homepage "https://github.com/Calvin-L/caltac"
-  url "https://github.com/Calvin-L/caltac/archive/5858ba84284ccf2eff2bbc13e46c151995605561.tar.gz"
+  url "https://github.com/Calvin-L/caltac/archive/80d7f6c99ed9fe240f8099b54a8643fc37a40c94.tar.gz"
   version "0.0.1"
-  sha256 "5e1c5a4d2ca7a50de84ad132b9c9c55001980916b2982da8d01d18280aa7304e"
+  sha256 "acf217678929a3ef612511a7994176122e6bfcbbde0ed8a0d23cb114b97757c0"
   license "MIT"
+  revision 1
 
   depends_on "coq"
   depends_on "coqhammer-tactics@1.3.2"
